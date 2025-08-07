b1dc7441313ad7bcd7c6031c008b3fb52d1513a7
[Faozia Fariha] <[faoziafariha058@gmail.com]>
2025-08-07 11:51:44 +0600

    This i smy web application

--- app.py
+++ app.py
@@ -27,11 +27,15 @@ def predict_api():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    data=[float(x) for x in request.form.values()]
-    final_input = scalar.transform(np.array(data).reshape(1, -1))
-    print(final_input)
-    output = model.predict(final_input)[0]
-    return render_template('home.html', prediction_text='The house price prediction is {}'.format(output))
+    data = request.get_json()
+
+    if not data or 'data' not in data:
+        return jsonify({'error': 'Invalid input'}), 400
+
+    # simulate prediction
+    prediction = 25.6  # dummy value
+
+    return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
